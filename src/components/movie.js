0938746cc1962be4acba609e30632e05f92d4beb
julianof03 <jlprod08@gmail.com>
2022-07-30 13:46:22 -0300

    displaySessions

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,24 +1,73 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect} from "react";
+import styled from "styled-components";
+
 export default function Movie(){
-    const [movie, SetMovie] = useState([]);
+    const [items, setItems] = useState([]);
     const { movieId } = useParams();
-    
     useEffect(() => {
-        const promise = axios.get(
-            `https://mock-api.driven.com.br/api/v7/cineflex/movies/${movieId}/showtimes`
-        );
-        promise.then((res) => {
-            SetMovie(res.data)
-        });
-    }, [movieId]);
-    const newarr = movie.days;
-    console.log(newarr);
+		const requisicao = axios.get(`https://mock-api.driven.com.br/api/v7/cineflex/movies/${movieId}/showtimes`);
+
+		requisicao.then(resposta => {
+			setItems(resposta.data.days);
+		});
+	}, []);
+    const newarr = [];
+    newarr.push(items);
+    console.log(items)
     return(
-        <div>         
-            {newarr.map(item => <div>{item.date}</div>)}
-        </div> 
- 
+        <Container>
+           {items.map(item => 
+            <div className="days">
+                <p>{item.weekday} - {item.date}</p>
+                <div>{item.showtimes.map(days => <div className="button">{days.name}</div>)}</div>
+            </div>)} 
+        </Container>
     );
-}                     
+}
+
+const Container = styled.div`
+width: 360px;
+height: 860px;
+color: #293b45;
+font-family: 'Roboto', sans-serif;
+
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: flex-start;
+align-content: center;
+
+overflow-y: scroll;
+div{
+    height: 83px;
+    width: 140px;
+    bottom:0px;
+    left: 0px;
+    display: flex;
+}
+.days{
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    height: 140px;
+    margin-left: 40px;
+    margin-bottom: 20px;
+    width: 220px;
+}
+.button{
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    margin-left: 8px;
+    margin-top: 26px;
+    width: 100px;
+    height: 43px;
+    color: #ffffff;
+    background-color: #E8833A;
+    border-radius: 2px;
+}
+
+`;
